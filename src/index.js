fe45ba9aa784a206197a0758d14e465b56c43288
pedro-drosa <pedro_drosa@hotmail.com>
2022-07-29 09:40:40 -0300

    feat: create a new user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
+function userExists(username) {
+  return users.some((user) => user.username === username);
+}
+
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const user = { id: uuidv4(), name, username, todos: [] };
+  if (userExists(username)) {
+    response.status(400).json({ error: "user already exists" });
+  }
+  users.push(user);
+  response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
